89c3989ecad43a10aa2af7331d9382727facd254
Daria TSenter <dasha.tsenter@gmail.com>
2018-04-12 17:06:24 -0400

    mongo for heroku

--- src/db.js
+++ src/db.js
@@ -26,4 +26,4 @@ const Log = new mongoose.Schema({
 module.exports = mongoose.model("Log", Log);
 
 
-mongoose.connect("mongodb://localhost/final");
+mongoose.connect("mongodb://heroku_bp8wct73:sm470cemnvagftcq0b1a6j0a1r@ds243049.mlab.com:43049/heroku_bp8wct73");
