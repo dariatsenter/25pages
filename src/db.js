ad67654d1eb68c48d0f7e17b49d04c4581d57013
Daria TSenter <dasha.tsenter@gmail.com>
2018-04-12 16:45:55 -0400

    setting up mongo for heroku

--- src/db.js
+++ src/db.js
@@ -26,24 +26,4 @@ const Log = new mongoose.Schema({
 module.exports = mongoose.model("Log", Log);
 
 
-// // is the environment variable, NODE_ENV, set to PRODUCTION? 
-// let dbconf;
-// if (process.env.NODE_ENV === 'PRODUCTION') {
-//  // if we're in PRODUCTION mode, then read the configuration from a file
-//  // use blocking file io to do this...
-//  const fs = require('fs');
-//  const path = require('path');
-//  const fn = path.join(__dirname, 'config.json');
-//  const data = fs.readFileSync(fn);
-
-//  // our configuration file will be in json, so parse it and set the
-//  // conenction string appropriately!
-//  const conf = JSON.parse(data);
-//  dbconf = conf.dbconf;
-// } else {
-//  // if we're not in PRODUCTION mode, then use
-//  dbconf = 'mongodb://localhost/dt1308';
-// }
-//mongoose.connect(dbconf);
-
-mongoose.connect("mongodb://localhost/final")
+mongoose.connect("mongodb://localhost/27017/final");
